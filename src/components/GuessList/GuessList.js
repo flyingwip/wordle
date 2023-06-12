4fbed3215e84de400dd18503a001523563135209
Martijn Wip <martijnwip@gmail.com>
2023-06-12 12:32:12 +0200

    add GuessList component showing the Guessed words

--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function GuessList({ guessedWord }) {
-  // const [wordList, setWordList] = useState([]);
+  const [wordList, setWordList] = useState([]);
 
-  console.log({ guessedWord });
+  // console.log({ guessedWord });
+
+  useEffect(() => {
+    if (guessedWord) {
+      setWordList((wordList) => [...wordList, guessedWord]);
+    }
+  }, [guessedWord]);
 
   return (
-    <div className="guess-results">
-      <p className="guess">FIRST</p>
-      <p className="guess">GUESS</p>
-      <p className="guess">{guessedWord}</p>
-    </div>
+    <ol className="guess-results">
+      {wordList.map((word, index) => {
+        return <li key={index}>{word}</li>;
+      })}
+    </ol>
   );
 }
 
